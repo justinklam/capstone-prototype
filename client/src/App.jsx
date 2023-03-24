d99043b485d0e348641bc959a6bd91b917eaa8d5
Justin Lam <justin.kh.lam@gmail.com>
2023-03-23 22:04:03 -0400

    install react-router-dom and set up base routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,39 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+
+// Components
+import Navbar from "./components/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
