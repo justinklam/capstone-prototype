33b902c6aa5a7f78109aba1a74a6a9ffd6510877
Justin Lam <justin.kh.lam@gmail.com>
2023-03-23 23:28:40 -0400

    set up mongoose connection to mongoDb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => console.log(`MongoDB Error:`, err));
 
 app.listen(8080, () => {
   console.log(`Express server is running on Port 8080!`);
